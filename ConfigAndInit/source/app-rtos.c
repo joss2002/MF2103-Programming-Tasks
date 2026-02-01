baacc82a7a9db155342f5abad3c02d33650a6711
joss2002 <josefinenyholm02@gmail.com>
2026-02-01 19:13:24 +0100

    fix(task2): corrected more formatting

--- ConfigAndInit/source/app-rtos.c
+++ ConfigAndInit/source/app-rtos.c
@@ -1,14 +1,13 @@
 /**
  * Handles the application, of controlling motor speed, through threads.
  *
- * @authors Josefine Nyholm, Nils Kiefer, Arun
+ * @authors Josefine Nyholm, Nils Kiefer, Arunraghavendren Senthil Kumar
  * 
  * @cite https://arm-software.github.io/CMSIS-RTX/latest/rtos2_tutorial.html#rtos2_tutorial_threads
  * @cite T. Martin and M. Rogers, The designer's guide to the cortex-m processor family, Second edition. 2016.
  */
 
 #include "main.h" 
-
 #include "application.h" 
 #include "controller.h"
 #include "peripherals.h"
@@ -17,35 +16,31 @@
 /* Global variables ----------------------------------------------------------*/
 int32_t reference, velocity, control;
 uint32_t millisec;
+osThreadId_t main_id, ctrl_id, ref_id; // Defines thread IDs
 
-osThreadId_t main_id, ctrl_id, ref_id;
-
-/* Functions -----------------------------------------------------------------*/
+/* Function/Thread declaration -----------------------------------------------*/
 
 void init_threads(void);
-	
-/* Threads -------------------------------------------------------------------*/
-
 static void app_main(void *arg);
 void app_ctrl(void *arg);
 void app_ref(void *arg);
 
 static const osThreadAttr_t threadAttr_main = {
 	.name = "app_main",
-	.stack_size = 128*4,				 // Application_Loop call + waiting for flags, small call-stack => 256 bytes, with margin => 512 bytes
-	.priority = osPriorityNormal
+	.stack_size = 128*4,         // Application_Loop call + waiting for flags, small call-stack => 256 bytes, with margin => 512 bytes
+	.priority   = osPriorityNormal
 };
 
 static const osThreadAttr_t threadAttr_ctrl = {
 	.name = "app_ctrl",
-	.stack_size = 128*4,         // ~24 bytes local variables, ~32 bytes RTOS-functions, ~232 bytes function calls, call-stack + safety ~100 bytes
-	.priority = osPriorityHigh
+	.stack_size = 128*4,       // ~24 bytes local variables, ~32 bytes RTOS-functions, ~232 bytes function calls, call-stack + safety ~100 bytes
+	.priority   = osPriorityHigh
 };
 
 static const osThreadAttr_t threadAttr_ref = {
 	.name = "app_ref",
-	.stack_size = 128*2,				 // ~8 bytes local variables, ~100-150 bytes RTOS-function, call-stack + safety ~100 bytes
-	.priority = osPriorityBelowNormal
+	.stack_size = 128*2,              // ~8 bytes local variables, ~100-150 bytes RTOS-function, call-stack + safety ~100 bytes
+	.priority   = osPriorityBelowNormal
 };
 
 /* Functions -----------------------------------------------------------------*/
@@ -57,17 +52,13 @@ void Application_Setup()
 {
   // Reset global variables
   reference = 2000;
-  velocity = 0;
-  control = 0;
-  millisec = 0;
-
-  // Initialise hardware
-  Peripheral_GPIO_EnableMotor();
-
-  // Initialize controller
-  Controller_Reset();
+  velocity  = 0;
+  control   = 0;
+  millisec  = 0;
 	
-	init_threads();
+  Peripheral_GPIO_EnableMotor(); // Initialise hardware
+  Controller_Reset();            // Initialize controller	
+	init_threads();                // Initializes threads
 }
 
 /**
@@ -75,31 +66,30 @@ void Application_Setup()
  */
 void Application_Loop()
  {
-   // Do nothing
    osThreadFlagsWait(0x01, osFlagsWaitAll, osWaitForever);
  }
  
 /* Thread Functions -----------------------------------------------------------*/
- 
- /**
- * Initializes threads
- */
+
+/**
+* Initializes threads
+*/
 void init_threads(void)
 {
 	osKernelInitialize();
+	
 	main_id = osThreadNew(app_main, NULL, &threadAttr_main);
 	ctrl_id = osThreadNew(app_ctrl, NULL, &threadAttr_ctrl);
 	ref_id  = osThreadNew(app_ref, NULL, &threadAttr_ref);
+	
 	osKernelStart();
 }
 
- /**
-  * Runs additional setup and calls Application_loop() indefinetly
-  */
+/**
+ * Calls Application_loop() indefinetly
+ */
 __NO_RETURN static void app_main(void *arg)
 {
-	/* Additional setup code... */
-	
 	for(;;)
 	{
 		Application_Loop();
@@ -111,21 +101,16 @@ __NO_RETURN static void app_main(void *arg)
  */
 __NO_RETURN void app_ctrl(void *arg)
 {
-	// Calculates amount of ticks representing the required period in ms
-	uint32_t tickDelay = (PERIOD_CTRL * osKernelGetTickFreq()) / 1000;
+	uint32_t tickDelay = (PERIOD_CTRL * osKernelGetTickFreq()) / 1000; // Calculates amount of ticks representing the required period in ms
 	
 	for(;;)
 	{
 		millisec = Main_GetTickMillisec();
 		
-		// Calculate motor velocity
-		velocity = Peripheral_Encoder_CalculateVelocity(millisec);
-
-		// Calculate control signal
-		control = Controller_PIController(&reference, &velocity, &millisec);
-
-		// Apply control signal to motor
-		Peripheral_PWM_ActuateMotor(control);
+		velocity = Peripheral_Encoder_CalculateVelocity(millisec);            // Calculate motor velocity
+		control  = Controller_PIController(&reference, &velocity, &millisec); // Calculate control signal
+		
+		Peripheral_PWM_ActuateMotor(control); // Apply control signal to motor
 		
 		uint32_t runTimeTicks = Main_GetTickMillisec() - millisec;
 		
@@ -143,10 +128,7 @@ __NO_RETURN void app_ref(void *arg)
 {
 	for(;;)
 	{
-		// Flip reference
-		reference = -reference;
-		
-		// Wait for next sample
-		osDelay(PERIOD_REF);
+		reference = -reference; // Flip reference
+		osDelay(PERIOD_REF); 	  // Wait for next sample
 	}
 }
